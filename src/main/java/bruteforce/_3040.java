97bd517de445e481a2dcfcc0140d4d3b7bc133e7
Hyeon-zero <gusdud9124@gmail.com>
2023-06-19 09:43:49 +0900

    Add bruteforce 3040

---
+++ src/main/java/bruteforce/_3040.java
@@ -0,0 +1,34 @@
+package bruteforce;
+import java.io.*;
+public class _3040 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        boolean[] check = new boolean[9];
+        int[] arr = new int[9];
+        int sum = 0;
+
+        for(int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            check[i] = true;
+            sum += arr[i];
+        }
+
+        for(int i = 0; i < 8; i++) {
+            for(int j = i + 1; j < 9; j++) {
+                if(arr[i] + arr[j] == sum - 100) {
+                    check[i] = false;
+                    check[j] = false;
+                }
+            }
+        }
+        for(int i = 0; i < 9; i++) {
+            if(check[i]) sb.append(arr[i] + "\n");
+        }
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+    }
+}
