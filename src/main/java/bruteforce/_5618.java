f9911f427d3b0d60991b70d2562bfe9da776e002
Hyeon-zero <gusdud9124@gmail.com>
2023-06-23 16:46:40 +0900

    Add bruteforce 5618

---
+++ src/main/java/bruteforce/_5618.java
@@ -0,0 +1,33 @@
+package bruteforce;
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+public class _5618 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+
+        if(n == 2) {
+            for(int i = 1; i <= arr[1]; i++) {
+                if(arr[1] % i == 0 && arr[2] % i == 0) sb.append(i + "\n");
+            }
+        }
+        if(n == 3) {
+            for(int i = 1; i <= arr[1]; i++) {
+                if(arr[1] % i == 0 && arr[2] % i == 0 && arr[3] % i == 0) sb.append(i + "\n");
+            }
+        }
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+    }
+}
