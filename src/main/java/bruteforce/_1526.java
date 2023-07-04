2debab558a47af51a177292026937ea43959ae37
Hyeon-zero <gusdud9124@gmail.com>
2023-07-04 15:03:54 +0900

    Add bruteforce 1526

---
+++ src/main/java/bruteforce/_1526.java
@@ -0,0 +1,31 @@
+package bruteforce;
+import java.io.*;
+public class _1526 {
+    static boolean check;
+    public static int maxNumber(int num) {
+        if(num == 0) return num;
+
+        if(num % 10 != 4 && num % 10 != 7) {
+            check = false;
+            return num;
+        }
+        return maxNumber(num / 10);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int max = 0;
+
+        for(int i = 4; i <= n; i++) {
+            check = true;
+            maxNumber(i);
+
+            if(check) max = Math.max(max, i);
+        }
+        bw.write(String.valueOf(max));
+        bw.flush();
+        bw.close();
+    }
+}
