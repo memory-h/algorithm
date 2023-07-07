7df7793201bba0388c91dfdf455329ba97ba1ee8
Hyeon-zero <gusdud9124@gmail.com>
2023-07-07 12:16:23 +0900

    Add implementation 28290

---
+++ src/main/java/implementation/_28290.java
@@ -0,0 +1,19 @@
+package implementation;
+import java.io.*;
+public class _28290 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        if(str.equals("fdsajkl;") || str.equals("jkl;fdsa")) bw.write("in-out");
+        else if(str.equals("asdf;lkj") || str.equals(";lkjasdf")) bw.write("out-in");
+        else if(str.equals("asdfjkl;")) bw.write("stairs");
+        else if(str.equals(";lkjfdsa")) bw.write("reverse");
+        else bw.write("molu");
+
+        bw.flush();
+        bw.close();
+    }
+}
