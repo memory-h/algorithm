935a64f8a849b4a4c821b1a8f6af7da7781ba089
Hyeon-zero <gusdud9124@gmail.com>
2023-07-10 14:09:29 +0900

    27433 팩토리얼 2

---
+++ src/main/java/math/_27433.java
@@ -0,0 +1,17 @@
+package math;
+import java.io.*;
+public class _27433 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        long factorial = 1;
+
+        for(int i = 1; i <= n; i++) factorial *= i;
+
+        bw.write(String.valueOf(factorial));
+        bw.flush();
+        bw.close();
+    }
+}
