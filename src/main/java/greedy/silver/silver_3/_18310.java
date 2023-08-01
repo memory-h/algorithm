030710301f5d54ff9525ee27399bf5daaf6b5568
Hyeon-zero <gusdud9124@gmail.com>
2023-08-01 11:46:32 +0900

    18310 안테나

---
+++ src/main/java/greedy/silver/silver_3/_18310.java
@@ -0,0 +1,23 @@
+package greedy.silver.silver_3;
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+public class _18310 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        bw.write(String.valueOf(arr[(n - 1) / 2 + 1]));
+        bw.flush();
+        bw.close();
+    }
+}
