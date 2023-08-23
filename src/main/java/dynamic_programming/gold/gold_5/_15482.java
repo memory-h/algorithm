51631e6c6dcbf3c2208023bb840002fd88b21ee7
Hyeon-zero <gusdud9124@gmail.com>
2023-08-23 17:39:31 +0900

    15482 한글 LCS

---
+++ src/main/java/dynamic_programming/gold/gold_5/_15482.java
@@ -0,0 +1,23 @@
+package dynamic_programming.gold.gold_5;
+import java.io.*;
+public class _15482 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[] c1 = br.readLine().toCharArray();
+        char[] c2 = br.readLine().toCharArray();
+
+        int[][] lcs = new int[c1.length + 1][c2.length + 1];
+
+        for(int i = 1; i <= c1.length; i++) {
+            for(int j = 1; j <= c2.length; j++) {
+                if(c1[i - 1] == c2[j - 1]) lcs[i][j] = lcs[i - 1][j - 1] + 1;
+                else lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
+            }
+        }
+        bw.write(String.valueOf(lcs[c1.length][c2.length]));
+        bw.flush();
+        bw.close();
+    }
+}
