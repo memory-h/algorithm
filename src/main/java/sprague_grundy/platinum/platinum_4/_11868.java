62763ed3ad3329eeb466bb66b3e3616bf01e18fc
Hyeon-zero <gusdud9124@gmail.com>
2023-11-15 08:53:32 +0900

    11868 님 게임 2

---
+++ src/main/java/sprague_grundy/platinum/platinum_4/_11868.java
@@ -0,0 +1,32 @@
+package sprague_grundy.platinum.platinum_4;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _11868 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int m = Integer.parseInt(br.readLine());
+        int grundy = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int value = Integer.parseInt(st.nextToken());
+
+            /*
+             각 칸마다 동전의 개수를 XOR 하여 값을 구한다.
+             (여러 게임을 동시에 진행하는 경우의 그런디 넘버는 각 게임판의 그런디 넘버를 XOR 한 값과 같다.)
+             */
+            grundy ^= value;
+        }
+
+        if (grundy != 0) bw.write("koosaga");
+        else bw.write("cubelover");
+
+        bw.flush();
+        bw.close();
+    }
+}
