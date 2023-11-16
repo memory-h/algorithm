1166ade3c364d5d85b57ab5130c8bb7587c90f30
Hyeon-zero <gusdud9124@gmail.com>
2023-11-16 12:18:20 +0900

    7685 Nim

---
+++ src/main/java/sprague_grundy/gold/gold_2/_7685.java
@@ -0,0 +1,49 @@
+package sprague_grundy.gold.gold_2;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _7685 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+
+            if (n == 0) break;
+
+            int[] arr = new int[n];
+            int grundy = 0;
+            int count = 0;
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+
+                grundy ^= arr[i];
+            }
+
+            if (grundy == 0) sb.append(0 + "\n");
+            else {
+                for (int i = 0; i < n; i++) {
+                    int temp = 0;
+
+                    for (int j = 0; j < n; j++) {
+                        if (i == j) continue;
+
+                        temp ^= arr[j];
+                    }
+                    if (temp <= arr[i]) count++;
+                }
+                sb.append(count).append("\n");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
