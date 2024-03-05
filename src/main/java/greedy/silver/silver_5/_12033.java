96ba9a9e031446aabac4787bca78bbbbc43c74f9
Hyeon-zero <gusdud9124@gmail.com>
2024-03-05 20:34:53 +0900

    12033 김인천씨의 식료품가게 (Small)

---
+++ src/main/java/greedy/silver/silver_5/_12033.java
@@ -0,0 +1,68 @@
+package greedy.silver.silver_5;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _12033 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int testCase = Integer.parseInt(br.readLine());
+        int x = 1;
+
+        while (testCase --> 0) {
+            int n = Integer.parseInt(br.readLine());
+
+            int[] arr = new int[2 * n];
+            int[] result = new int[n];
+            boolean[] visited = new boolean[2 * n];
+
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < 2 * n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int index = 0;
+
+            for (int i = 0; i < 2 * n; i++) {
+                if (index == n) {
+                    break;
+                }
+
+                // 방문했거나, 정상 가격인 경우
+                if (visited[i] || arr[i] % 3 != 0) {
+                    continue;
+                }
+
+                long price = (arr[i] / 3L) * 4; // 정상 가격
+
+                for (int j = i + 1; j < 2 * n; j++) {
+                    if (!visited[j] && price == arr[j]) {
+                        visited[j] = true;
+                        result[index] = arr[i]; // 할인 가격 저장
+                        index++;
+
+                        break;
+                    }
+                }
+            }
+
+            sb.append("Case #").append(x).append(": ");
+
+            for (int i : result) {
+                sb.append(i).append(" ");
+            }
+
+            sb.append("\n");
+            x++;
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
