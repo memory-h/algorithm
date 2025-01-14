5531c633a02b2bbce98e71640df17fe36de4d31b
Hyeon Young Jang <gusdud9124@gmail.com>
2025-01-14 15:20:56 +0900

    30088 공포의 면담실

---
+++ src/main/java/prefix_sum/silver/silver_5/_30088.java
@@ -0,0 +1,42 @@
+package prefix_sum.silver.silver_5;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _30088 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        long[] times = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            long employee = Integer.parseInt(st.nextToken());
+            long DepartmentTime = 0;
+
+            for (int j = 0; j < employee; j++) {
+                DepartmentTime += Integer.parseInt(st.nextToken());
+            }
+            times[i] = DepartmentTime;
+        }
+        Arrays.sort(times);
+
+        long[] prefixSum = new long[n + 1];
+        long result = 0;
+
+        for (int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + times[i];
+            result += prefixSum[i + 1];
+        }
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+    }
+
+}
