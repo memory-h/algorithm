0e8d15cd5cf6b5ac82cfcb35056f7540fff30465
memory-h <gusdud9124@gmail.com>
2025-08-01 20:54:35 +0900

    10162 전자레인지

--- src/main/java/greedy/bronze/bronze_3/_10162.java
+++ src/main/java/greedy/bronze/bronze_3/_10162.java
@@ -1,28 +1,27 @@
 package greedy.bronze.bronze_3;
-import java.util.Scanner;
+
+import java.io.*;
+
 public class _10162 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int T = sc.nextInt();   // 5분, 1분, 10초
-        int A = 0;
-        int B = 0;
-        int C = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        A += T / 300;
-        T %= 300;
-        B += T / 60;
-        T %= 60;
-        C += T / 10;
-        T %= 10;
+        int t = Integer.parseInt(br.readLine());
 
-        if(T == 0){
-            System.out.println(A + " " + B + " " + C);
-        }
-        else{
-            System.out.println("-1");
+        int[] timeBtn = new int[] {300, 60, 10};
+
+        for (int val : timeBtn) {
+            sb.append(t / val)
+                    .append(" ");
+            t %= val;
         }
 
-        sc.close();
+        bw.write(t == 0 ? sb.toString() : "-1");
+        bw.flush();
+        bw.close();
     }
+
 }
