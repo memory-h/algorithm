19bc505a2cb342c38c2dcade5f686f01ca1a3a57
memory-h <gusdud9124@gmail.com>
2025-08-03 12:18:48 +0900

    5585 거스름돈

--- src/main/java/greedy/bronze/bronze_2/_5585.java
+++ src/main/java/greedy/bronze/bronze_2/_5585.java
@@ -1,21 +1,27 @@
 package greedy.bronze.bronze_2;
-import java.util.Scanner;
+
+import java.io.*;
+
 public class _5585 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int result = 0;
-        int sum = 0;
-        sum = 1000 - N;
-        int[] arr = {500, 100, 50, 10, 5, 1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i = 0; i < arr.length; i++){
-            result += sum / arr[i];
-            sum %= arr[i];
+        int payment = Integer.parseInt(br.readLine());
+        int change = 1000 - payment;
+
+        int[] money = new int[] {500, 100, 50, 10, 5, 1};
+        int count = 0;
+
+        for (int m : money) {
+            count += change / m;
+            change %= m;
         }
-        sc.close();
 
-        System.out.println(result);
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
     }
+
 }
