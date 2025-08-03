812c30f412923653be709b3a58f69fb716d21696
memory-h <gusdud9124@gmail.com>
2025-08-03 12:10:42 +0900

    2864 5와 6의 차이

--- src/main/java/greedy/bronze/bronze_2/_2864.java
+++ src/main/java/greedy/bronze/bronze_2/_2864.java
@@ -1,50 +1,24 @@
 package greedy.bronze.bronze_2;
-import java.util.Scanner;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class _2864 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        String a = sc.next();
-        String b = sc.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        sc.close();
+        String a = st.nextToken();
+        String b = st.nextToken();
 
-        StringBuilder minA = new StringBuilder();
-        StringBuilder maxA = new StringBuilder();
-        StringBuilder minB = new StringBuilder();
-        StringBuilder maxB = new StringBuilder();
+        int min = Integer.parseInt(a.replace('6', '5')) + Integer.parseInt(b.replace('6', '5'));
+        int max = Integer.parseInt(a.replace('5', '6')) + Integer.parseInt(b.replace('5', '6'));
 
-        for(int i = 0; i < a.length(); i++){
-            if(a.charAt(i) == '5'){
-                maxA.append("6");
-            }
-            else{
-                maxA.append(a.charAt(i));
-            }
-            if(a.charAt(i) == '6'){
-                minA.append("5");
-            }
-            else{
-                minA.append(a.charAt(i));
-            }
-        }
-        for(int i = 0; i < b.length(); i++){
-            if(b.charAt(i) == '5'){
-                maxB.append("6");
-            }
-            else{
-                maxB.append(b.charAt(i));
-            }
-            if(b.charAt(i) == '6'){
-                minB.append("5");
-            }
-            else{
-                minB.append(b.charAt(i));
-            }
-        }
-        int min = Integer.parseInt(minA.toString()) + Integer.parseInt(minB.toString());
-        int max = Integer.parseInt(maxA.toString()) + Integer.parseInt(maxB.toString());
-
-        System.out.println(min + " " + max);
+        bw.write(min + " " + max);
+        bw.flush();
+        bw.close();
     }
+
 }
