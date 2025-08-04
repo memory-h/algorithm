5530f93d8ee492929acf72d8640e95a4589b4ea3
memory-h <gusdud9124@gmail.com>
2025-08-04 19:04:48 +0900

    14487 욱제는 효도쟁이야!!

--- src/main/java/greedy/bronze/bronze_2/_14487.java
+++ src/main/java/greedy/bronze/bronze_2/_14487.java
@@ -1,23 +1,31 @@
 package greedy.bronze.bronze_2;
-import java.util.Scanner;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class _14487 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();   // 마을의 수
-        int[] v = new int[n];
-        int max = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] cost = new int[n];
         int sum = 0;
+        int max = 0;
 
-        for(int i = 0; i < n; i++){
-            v[i] = sc.nextInt();   // 마을 이동 비용
-            sum += v[i];
-            if(v[i] > max){
-                max = v[i];
-            }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            cost[i] = Integer.parseInt(st.nextToken());
+            sum += cost[i];
+            max = Math.max(max, cost[i]);
         }
-        sc.close();
 
-        System.out.println(sum - max);
+        bw.write(String.valueOf(sum - max));
+        bw.flush();
+        bw.close();
     }
+
 }
