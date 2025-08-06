a7af17be6abbb186ad4d8eebcae6e880e306b7ca
memory-h <gusdud9124@gmail.com>
2025-08-06 21:42:04 +0900

    22864 피로도

--- src/main/java/greedy/bronze/bronze_2/_22864.java
+++ src/main/java/greedy/bronze/bronze_2/_22864.java
@@ -4,44 +4,37 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class _22864 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int a = Integer.parseInt(st.nextToken()); // 피로도 추가
-        int b = Integer.parseInt(st.nextToken()); // 일
-        int c = Integer.parseInt(st.nextToken()); // 피로도 감소
-        int m = Integer.parseInt(st.nextToken()); // 피로도 max 값
+        int a = Integer.parseInt(st.nextToken()); // 피로도 ↑
+        int b = Integer.parseInt(st.nextToken()); // 일 처리량
+        int c = Integer.parseInt(st.nextToken()); // 피로도 ↓
+        int m = Integer.parseInt(st.nextToken()); // 최대 피로도
 
-        if (a > m) {
-            bw.write("0");
-            bw.flush();
-            bw.close();
-
-            return;
-        }
-
-        int time = 0;
         int work = 0;
-        int fatigability = 0; // 피로도
+        int time = 0;
+        int fatigueLevel = 0;
 
         while (time < 24) {
-            if (fatigability + a <= m) {
-                fatigability += a;
+            if (fatigueLevel + a <= m) {
+                fatigueLevel += a;
                 work += b;
-                time++;
+            } else {
+                fatigueLevel -= c;
+                if (fatigueLevel < 0) {
+                    fatigueLevel = 0;
+                }
             }
-            if (fatigability + a > m) {
-                fatigability -= c;
-                time++;
-
-                if (fatigability < 0) fatigability = 0;
-            }
+            time++;
         }
 
         bw.write(String.valueOf(work));
         bw.flush();
         bw.close();
     }
+
 }
