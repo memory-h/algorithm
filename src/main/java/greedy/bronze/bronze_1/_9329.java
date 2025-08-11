142914094b9b379a4796d196e1bd351daf972a3c
memory-h <gusdud9124@gmail.com>
2025-08-11 22:03:31 +0900

    9329 패스트 푸드 상금

--- src/main/java/greedy/bronze/bronze_1/_9329.java
+++ src/main/java/greedy/bronze/bronze_1/_9329.java
@@ -11,57 +11,51 @@ public class _9329 {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        int testcase = Integer.parseInt(br.readLine());
+        int testCases = Integer.parseInt(br.readLine());
 
-        while (testcase --> 0) {
+        while (testCases-- > 0) {
             st = new StringTokenizer(br.readLine());
 
             int n = Integer.parseInt(st.nextToken()); // 상금의 종류
             int m = Integer.parseInt(st.nextToken()); // 스티커의 종류
 
-            int[][] arr = new int[n][m + 1]; // 각 상금에 필요한 스티커 정보
+            int[][] stickers = new int[n][m + 1];
+            int[] prizeMonies = new int[n];
 
             for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
-                int k = Integer.parseInt(st.nextToken());
+
+                int k = Integer.parseInt(st.nextToken()); // 필요한 스티커의 개수
 
                 for (int j = 0; j < k; j++) {
-                    int sticker = Integer.parseInt(st.nextToken()); // 스티커 종류
-                    arr[i][sticker] = 1; // 해당 종류의 스티커가 필요함을 표시
+                    int stocker = Integer.parseInt(st.nextToken());
+                    stickers[i][stocker] = 1;
                 }
 
-                arr[i][0] = Integer.parseInt(st.nextToken()); // 상금의 액수
+                prizeMonies[i] = Integer.parseInt(st.nextToken()); // 상금
             }
 
-            int[] coach = new int[m + 1];
+            int[] stickerCounts = new int[m + 1];
 
             st = new StringTokenizer(br.readLine());
-
-            // 코치가 가지고 있는 스티커
             for (int i = 1; i <= m; i++) {
-                coach[i] = Integer.parseInt(st.nextToken());
+                stickerCounts[i] = Integer.parseInt(st.nextToken());
             }
 
-            int result = 0;
+            int amount = 0;
 
             for (int i = 0; i < n; i++) {
                 int minCount = Integer.MAX_VALUE;
 
                 for (int j = 1; j <= m; j++) {
-                    // 필요한 스티커인 경우
-                    if (arr[i][j] == 1) {
-                        // 최소 교환 횟수를 구한다.
-                        minCount = Math.min(minCount, coach[j]);
+                    if (stickers[i][j] == 1) {
+                        minCount = Math.min(minCount, stickerCounts[j]);
                     }
                 }
-
-                // 상금을 교환할 수 있는 경우
-                if (minCount > 0) {
-                    result += minCount * arr[i][0];
-                }
+                amount += prizeMonies[i] * minCount;
             }
 
-            sb.append(result).append("\n");
+            sb.append(amount).append("\n");
         }
 
         bw.write(sb.toString());
