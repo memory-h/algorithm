dfb4812f6b0a2b65a10e15d3c7d2b629f457f94f
memory-h <gusdud9124@gmail.com>
2025-08-15 18:40:29 +0900

    14659 한조서열정리하고옴ㅋㅋ

--- src/main/java/greedy/bronze/bronze_1/_14659.java
+++ src/main/java/greedy/bronze/bronze_1/_14659.java
@@ -1,31 +1,42 @@
 package greedy.bronze.bronze_1;
-import java.util.Scanner;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class _14659 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int[] arr = new int[N];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        for(int i = 0; i < N; i++){
-            arr[i] = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int count = 0;
-        int result = Integer.MIN_VALUE;
 
-        for(int i = 0; i < N; i++){
-            count = 0;
-            for(int j = i + 1; j < N; j++){
-                if(arr[i] > arr[j]){
+        int maxCount = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            int count = 0;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
                     count++;
-                }
-                else{
+                } else {
                     break;
                 }
             }
-            result = Math.max(result, count);
+            maxCount = Math.max(count, maxCount);
         }
-        sc.close();
-        System.out.println(result);
+
+        bw.write(String.valueOf(maxCount));
+        bw.flush();
+        bw.close();
     }
+
 }
