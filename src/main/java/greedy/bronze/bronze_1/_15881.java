e273765ee30df6c5c6618c0928208a001b25b801
memory-h <gusdud9124@gmail.com>
2025-08-17 19:02:43 +0900

    15881 Pen Pineapple Apple Pen

--- src/main/java/greedy/bronze/bronze_1/_15881.java
+++ src/main/java/greedy/bronze/bronze_1/_15881.java
@@ -3,26 +3,27 @@ package greedy.bronze.bronze_1;
 import java.io.*;
 
 public class _15881 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        String input = br.readLine();
+        String obj = br.readLine();
 
-        String str = "pPAp";
-        int count = 0;
+        String ppap = "pPAp";
+        int ppapCount = 0;
 
-        // substring() 메소드를 사용하여 펜-파인애플-애플-펜의 최대 개수를 구한다.
-        for (int i = 0; i <= input.length() - 4; i++) {
-            if (input.substring(i, i + 4).equals(str)) {
+        for (int i = 0; i <= n - 4; i++) {
+            if (obj.substring(i, i + 4).equals(ppap)) {
                 i += 3;
-                count++;
+                ppapCount++;
             }
         }
 
-        bw.write(String.valueOf(count));
+        bw.write(String.valueOf(ppapCount));
         bw.flush();
         bw.close();
     }
+
 }
