df3d4acae814d2584034b5869fb025211f35bcf6
memory-h <gusdud9124@gmail.com>
2025-08-21 21:56:24 +0900

    1343 폴리오미노

--- src/main/java/greedy/silver/silver_5/_1343.java
+++ src/main/java/greedy/silver/silver_5/_1343.java
@@ -1,28 +1,30 @@
 package greedy.silver.silver_5;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
+
 public class _1343 {
-    public static String poliomino(String s) {
-        String str = "";
-        String A = "AAAA";
-        String B = "BB";
 
-        s = s.replaceAll("XXXX", A);
-        str = s.replaceAll("XX", B);
-
-        if (str.contains("X")) {
-            str = "-1";
-        }
-        return str;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String s = br.readLine();
+        String board = br.readLine();
+        String polyominoA = "AAAA";
+        String polyominoB = "BB";
 
-        System.out.println(poliomino(s));
+        board = board.replaceAll("XXXX", polyominoA);
+        board = board.replaceAll("XX", polyominoB);
+
+        if (board.contains("X")) {
+            bw.write("-1");
+        } else {
+            bw.write(board);
+        }
+
+        bw.flush();
+        bw.close();
     }
+
 }
 // replaceAll() : 대상 문자열을 원하는 문자 값으로 변환하는 함수이다.
 // 첫번째 매개변수는 변환하고자 하는 대상이 될 문자열
