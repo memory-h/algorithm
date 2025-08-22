0f97e3c765a5b1cee015604fdee259bc7c1e4a1d
memory-h <gusdud9124@gmail.com>
2025-08-22 17:25:21 +0900

    1417 국회의원 선거

--- src/main/java/greedy/silver/silver_5/_1417.java
+++ src/main/java/greedy/silver/silver_5/_1417.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class _1417 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -15,18 +16,21 @@ public class _1417 {
         // 우선순위 큐를 내림차순으로 정렬한다.
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
-        for (int i = 0; i < n - 1; i++) pq.add(Integer.parseInt(br.readLine()));
+        for (int i = 0; i < n - 1; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
 
-        int count = 0;
+        int minCount = 0;
 
         while (!pq.isEmpty() && pq.peek() >= dasom) {
             dasom++;
-            count++;
+            minCount++;
             pq.add(pq.poll() - 1);
         }
 
-        bw.write(String.valueOf(count));
+        bw.write(String.valueOf(minCount));
         bw.flush();
         bw.close();
     }
+
 }
