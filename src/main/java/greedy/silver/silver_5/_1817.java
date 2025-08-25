11fed52304d01a2514d1134de7583c1f908d1d33
memory-h <gusdud9124@gmail.com>
2025-08-25 15:27:38 +0900

    1817 짐 챙기는 숌

--- src/main/java/greedy/silver/silver_5/_1817.java
+++ src/main/java/greedy/silver/silver_5/_1817.java
@@ -1,7 +1,10 @@
 package greedy.silver.silver_5;
+
 import java.io.*;
 import java.util.StringTokenizer;
+
 public class _1817 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -9,26 +12,28 @@ public class _1817 {
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        int boxCount = 0;
 
-        int[] arr = new int[n + 1];
-        int count = 0, cache = 0;
+        if (n > 0) {
+            boxCount = 1;
+            int curBoxWeight = m;
 
-        if(n > 0) {
-            count = 1;
             st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                int bookWeight = Integer.parseInt(st.nextToken());
 
-            for (int i = 1; i <= n; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
-                cache += arr[i];
-
-                if (cache > m) {
-                    count++;
-                    cache = arr[i];
+                if (curBoxWeight >= bookWeight) {
+                    curBoxWeight -= bookWeight;
+                } else {
+                    curBoxWeight = m - bookWeight;
+                    boxCount++;
                 }
             }
         }
-        bw.write(String.valueOf(count));
+
+        bw.write(String.valueOf(boxCount));
         bw.flush();
         bw.close();
     }
+
 }
