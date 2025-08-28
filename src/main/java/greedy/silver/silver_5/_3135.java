83325b7818d6e749a430d9a81493ab7db8645015
memory-h <gusdud9124@gmail.com>
2025-08-28 12:29:55 +0900

    3135 라디오

--- src/main/java/greedy/silver/silver_5/_3135.java
+++ src/main/java/greedy/silver/silver_5/_3135.java
@@ -1,7 +1,10 @@
 package greedy.silver.silver_5;
+
 import java.io.*;
 import java.util.StringTokenizer;
+
 public class _3135 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -11,17 +14,16 @@ public class _3135 {
         int b = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
+        int minBtnCount = Math.abs(a - b);
 
-        for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int frequency = Integer.parseInt(br.readLine());
+            minBtnCount = Math.min(minBtnCount, Math.abs(frequency - b) + 1);
+        }
 
-        int frequency = Math.abs(a - b);
-        int min = 1000;
-
-        for(int i = 0; i < n; i++) min = Math.min(min, Math.abs(b - arr[i]));
-
-        bw.write(String.valueOf(Math.min(frequency, min + 1)));
+        bw.write(String.valueOf(minBtnCount));
         bw.flush();
         bw.close();
     }
+
 }
