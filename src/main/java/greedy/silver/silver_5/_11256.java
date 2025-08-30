b052a2d7d90764837d485eee1f7cf0995c5057a7
memory-h <gusdud9124@gmail.com>
2025-08-30 12:45:25 +0900

    11256 사탕

--- src/main/java/greedy/silver/silver_5/_11256.java
+++ src/main/java/greedy/silver/silver_5/_11256.java
@@ -1,44 +1,49 @@
 package greedy.silver.silver_5;
+
 import java.io.*;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
+
 public class _11256 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        int testCase = Integer.parseInt(br.readLine());
+        int testCases = Integer.parseInt(br.readLine());
 
-        while(testCase --> 0) {
+        while (testCases-- > 0) {
             st = new StringTokenizer(br.readLine());
 
-            int candy = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
             int n = Integer.parseInt(st.nextToken());
-            Integer[] arr = new Integer[n];
 
-            for(int i = 0; i < n; i++) {
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
-
-                int r = Integer.parseInt(st.nextToken());
-                int c = Integer.parseInt(st.nextToken());
-
-                arr[i] = r * c;
+                arr[i] = Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
             }
-            Arrays.sort(arr, Collections.reverseOrder());
-            int count = 0;
 
-            for(int i = 0; i < n; i++) {
-                if(candy <= 0) break;
-                candy -= arr[i];
-                count++;
+            Arrays.sort(arr);
+
+            int boxCount = 0;
+
+            for (int i = arr.length - 1; i >= 0; i--) {
+                if (j > 0) {
+                    j -= arr[i];
+                    boxCount++;
+                }
             }
-            sb.append(count + "\n");
+
+            sb.append(boxCount).append("\n");
         }
+
         bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
+
 }
