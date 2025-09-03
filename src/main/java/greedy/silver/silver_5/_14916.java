27dccd9cc012e80edb812280a49a6964d21d2403
memory-h <gusdud9124@gmail.com>
2025-09-03 17:45:18 +0900

    14916 거스름돈

--- src/main/java/greedy/silver/silver_5/_14916.java
+++ src/main/java/greedy/silver/silver_5/_14916.java
@@ -1,23 +1,28 @@
 package greedy.silver.silver_5;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
+
 public class _14916 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        int cnt = 0;
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
 
-        while(N > 0){
-            if(N % 5 == 0){
-                cnt = N / 5 + cnt;
+        while (true) {
+            if (n % 5 == 0) {
+                count += n / 5;
                 break;
             }
-            N -= 2;
-            cnt++;
+            n -= 2;
+            count++;
         }
-        if(N < 0) System.out.println(-1);
-        else System.out.println(cnt);
+
+        bw.write(n < 0 ? "-1" : String.valueOf(count));
+        bw.flush();
+        bw.close();
     }
+
 }
