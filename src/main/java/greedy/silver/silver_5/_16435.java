2838e789d99d1ba24e3c4802f8fcd2d1219c4e15
memory-h <gusdud9124@gmail.com>
2025-09-07 15:41:03 +0900

    16435 스네이크버드

--- src/main/java/greedy/silver/silver_5/_16435.java
+++ src/main/java/greedy/silver/silver_5/_16435.java
@@ -1,26 +1,37 @@
 package greedy.silver.silver_5;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+
 public class _16435 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new  StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int L = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
+        int[] fruits = new int[n];
 
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(arr);
-
-        for(int i = 0; i < N; i++) {
-            if(L >= arr[i]) L++;
+        st = new  StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            fruits[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(L);
+
+        Arrays.sort(fruits);
+
+        for (int i = 0; i < n; i++) {
+            if (l >= fruits[i]) {
+                l++;
+            }
+        }
+
+        bw.write(String.valueOf(l));
+        bw.flush();
+        bw.close();
     }
+
 }
