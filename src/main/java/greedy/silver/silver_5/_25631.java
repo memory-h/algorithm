2a2e24e6906b48d9d735d9e39a5098e0f3582fcf
memory-h <gusdud9124@gmail.com>
2025-09-09 02:51:18 +0900

    25631 마트료시카 합치기

--- src/main/java/greedy/silver/silver_5/_25631.java
+++ src/main/java/greedy/silver/silver_5/_25631.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class _25631 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -12,30 +13,32 @@ public class _25631 {
 
         int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
+        int[] dolls = new int[n];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            dolls[i] = Integer.parseInt(st.nextToken());
+        }
 
-        Arrays.sort(arr);
+        Arrays.sort(dolls);
 
-        int temp = arr[0];
-        int count = 1, max = 1;
+        int currentValue = dolls[0];
+        int maxFrequency = Integer.MIN_VALUE;
+        int frequency = 0;
 
-        // 크기가 같은 마트료시카 인형의 개수의 최댓값을 찾는다.
-        for (int i = 1; i < n; i++) {
-            if (temp == arr[i]) {
-                count++;
-                max = Math.max(max, count);
-            }
-            else {
-                temp = arr[i];
-                count = 1;
+        for (int i = 0; i < n; i++) {
+            if (currentValue == dolls[i]) {
+                frequency++;
+            } else {
+                maxFrequency = Math.max(maxFrequency, frequency);
+                currentValue = dolls[i];
+                frequency = 1;
             }
         }
 
-        bw.write(String.valueOf(max));
+        bw.write(String.valueOf(Math.max(maxFrequency, frequency)));
         bw.flush();
         bw.close();
     }
+
 }
