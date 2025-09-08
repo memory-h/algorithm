e5318299d28d604583e5768c86f6a9d70520320d
memory-h <gusdud9124@gmail.com>
2025-09-08 12:46:36 +0900

    25496 장신구 명장 임스

--- src/main/java/greedy/silver/silver_5/_25496.java
+++ src/main/java/greedy/silver/silver_5/_25496.java
@@ -5,31 +5,36 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class _25496 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new  StringTokenizer(br.readLine());
 
         int p = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
+        int[] fatigueLevel = new int[n];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            fatigueLevel[i] = Integer.parseInt(st.nextToken());
+        }
 
-        Arrays.sort(arr);
+        Arrays.sort(fatigueLevel);
+
         int count = 0;
 
         for (int i = 0; i < n; i++) {
-            if (p >= 200) break;
-
-            p += arr[i];
-            count++;
+            if (p < 200) {
+                p += fatigueLevel[i];
+                count++;
+            }
         }
 
         bw.write(String.valueOf(count));
         bw.flush();
         bw.close();
     }
+
 }
