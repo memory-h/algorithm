dda295d371330334db712028c777b6fcf0ca1776
memory-h <gusdud9124@gmail.com>
2025-09-13 18:56:21 +0900

    30088 공포의 면담실

--- src/main/java/prefix_sum/silver/silver_5/_30088.java
+++ src/main/java/prefix_sum/silver/silver_5/_30088.java
@@ -12,31 +12,71 @@ public class _30088 {
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
+
         long[] times = new long[n];
+        long[] prefixSum = new long[n + 1];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            long employee = Integer.parseInt(st.nextToken());
-            long DepartmentTime = 0;
+
+            int employee = Integer.parseInt(st.nextToken());
+            long sum = 0L;
 
             for (int j = 0; j < employee; j++) {
-                DepartmentTime += Integer.parseInt(st.nextToken());
+                sum += Integer.parseInt(st.nextToken());
             }
-            times[i] = DepartmentTime;
+
+            times[i] = sum;
         }
+
         Arrays.sort(times);
 
-        long[] prefixSum = new long[n + 1];
-        long result = 0;
-
         for (int i = 0; i < n; i++) {
-            prefixSum[i + 1] = prefixSum[i] + times[i];
-            result += prefixSum[i + 1];
+            prefixSum[i + 1] = times[i] + prefixSum[i];
+        }
+
+        long result = 0L;
+
+        for (long val : prefixSum) {
+            result += val;
         }
 
         bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
     }
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st;
+//
+//        int n = Integer.parseInt(br.readLine());
+//        long[] times = new long[n];
+//
+//        for (int i = 0; i < n; i++) {
+//            st = new StringTokenizer(br.readLine());
+//            long employee = Integer.parseInt(st.nextToken());
+//            long DepartmentTime = 0;
+//
+//            for (int j = 0; j < employee; j++) {
+//                DepartmentTime += Integer.parseInt(st.nextToken());
+//            }
+//            times[i] = DepartmentTime;
+//        }
+//        Arrays.sort(times);
+//
+//        long[] prefixSum = new long[n + 1];
+//        long result = 0;
+//
+//        for (int i = 0; i < n; i++) {
+//            prefixSum[i + 1] = prefixSum[i] + times[i];
+//            result += prefixSum[i + 1];
+//        }
+//
+//        bw.write(String.valueOf(result));
+//        bw.flush();
+//        bw.close();
+//    }
 
 }
