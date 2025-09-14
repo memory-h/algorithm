9ecff7fffbe260f930f866414576446ae30eca38
memory-h <gusdud9124@gmail.com>
2025-09-14 17:26:56 +0900

    16173 점프왕 쩰리 (Small)

--- src/main/java/dfs/silver/silver_4/_16173.java
+++ src/main/java/dfs/silver/silver_4/_16173.java
@@ -5,38 +5,9 @@ import java.util.StringTokenizer;
 
 public class _16173 {
 
-    static int n;
-    static boolean result;
-    static int[][] graph;
-    static boolean[][] visited;
-    static int[] dx = {0, 1};
-    static int[] dy = {1, 0};
-
-    private static void dfs(int x, int y) {
-
-        // ‘쩰리’가 끝 점에 도달했을 때
-        if (graph[x][y] == -1) {
-            result = true;
-
-            return;
-        }
-
-        // '쩰리’가 이동 가능한 방향은 오른쪽과 아래
-        for (int i = 0; i < 2; i++) {
-            int nx = dx[i] * graph[x][y] + x;
-            int ny = dy[i] * graph[x][y] + y;
-
-            // 게임 구역을 벗어나거나, 이미 방문한 구역일 때
-            if (nx >= n || ny >= n || visited[nx][ny]) {
-                continue;
-            }
-
-            visited[nx][ny] = true;
-
-            dfs(nx, ny);
-        }
-
-    }
+    private static int n;
+    private static int[][] arr;
+    private static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -45,23 +16,45 @@ public class _16173 {
 
         n = Integer.parseInt(br.readLine());
 
-        graph = new int[n][n];
+        arr = new int[n][n];
         visited = new boolean[n][n];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
 
             for (int j = 0; j < n; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        dfs(0, 0);
+        visited[0][0] = true;
+        boolean result = dfs(0, 0);
 
-        if (result) bw.write("HaruHaru");
-        else bw.write("Hing");
-
+        bw.write(result ? "HaruHaru" : "Hing");
         bw.flush();
         bw.close();
     }
+
+    private static boolean dfs(int x, int y) {
+        if (x == n - 1 && y == n - 1) {
+            return true;
+        }
+
+        if (x + arr[x][y] < n && !visited[x + arr[x][y]][y]) {
+            visited[x + arr[x][y]][y] = true;
+            if (dfs(x + arr[x][y], y)) {
+                return true;
+            }
+        }
+
+        if (y + arr[x][y] < n && !visited[x][y + arr[x][y]]) {
+            visited[x][y + arr[x][y]] = true;
+            if (dfs(x, y + arr[x][y])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
