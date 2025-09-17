a77f595e8092213f4293bc8ff0f1d14a9c43464d
memory-h <gusdud9124@gmail.com>
2025-09-17 21:30:15 +0900

    31575 도시와 비트코인

--- src/main/java/bfs/silver/silver_3/_31575.java
+++ src/main/java/bfs/silver/silver_3/_31575.java
@@ -7,12 +7,14 @@ import java.util.StringTokenizer;
 
 public class _31575 {
 
-    static int n, m;
-    static int[][] graph;
-    static boolean[][] visited;
+    private static int n;
+    private static int m;
 
-    static int[] dx = {1, 0};
-    static int[] dy = {0, 1};
+    private static int[][] graph;
+    private static boolean[][] visited;
+
+    private static int[] dx = {1, 0};
+    private static int[] dy = {0, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,28 +35,25 @@ public class _31575 {
             }
         }
 
-        boolean result = bfs();
+        String result = bfs();
 
-        bw.write(result ? "Yes" : "No");
+        bw.write(result);
         bw.flush();
         bw.close();
     }
 
-    private static boolean bfs() {
-
+    private static String bfs() {
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0});
+        queue.offer(new int[]{0, 0});
         visited[0][0] = true;
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
 
-            // 진우가 거래소에 도착한 경우
             if (cur[0] == m - 1 && cur[1] == n - 1) {
-                return true;
+                return "Yes";
             }
 
-            // 동쪽, 남쪽으로 이동
             for (int i = 0; i < 2; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
@@ -63,15 +62,13 @@ public class _31575 {
                     continue;
                 }
 
-                // 방문하지 않았고, 진우가 갈 수 있는 칸인 경우
                 if (!visited[nx][ny] && graph[nx][ny] == 1) {
                     visited[nx][ny] = true;
-                    queue.add(new int[]{nx, ny});
+                    queue.offer(new int[]{nx, ny});
                 }
             }
         }
-
-        return false;
+        return "No";
     }
 
 }
