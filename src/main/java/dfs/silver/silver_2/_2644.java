6915597fe9fb0b873c0e09dfba36decf43b79102
memory-h <gusdud9124@gmail.com>
2025-09-20 00:19:28 +0900

    2644 촌수계산

--- src/main/java/dfs/silver/silver_2/_2644.java
+++ src/main/java/dfs/silver/silver_2/_2644.java
@@ -1,55 +1,63 @@
 package dfs.silver.silver_2;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+
+import java.io.*;
 import java.util.StringTokenizer;
+
 public class _2644 {
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
-    static boolean[] visited;
-    static int N, M, x, y, cnt = -1;
+    private static int n;
+    private static int end;
+    private static int result = -1;
 
-    public static void dfs(int startNode, int find, int n) {
-        visited[startNode] = true;
+    private static int[][] graph;
+    private static boolean[] visited;
 
-        for(int node : graph.get(startNode)) {
-            if(!visited[node]) {
-                if(node == find) {
-                    cnt = n + 1;
-                    return;
-                }
-                dfs(node, find, n + 1);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
+
+        int m = Integer.parseInt(br.readLine());
+
+        graph = new int[n + 1][n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            graph[x][y] = 1;
+            graph[y][x] = 1;
+        }
+
+        dfs(start, 0);
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+    }
+
+    private static void dfs(int node, int depth) {
+        visited[node] = true;
+
+        if (node == end) {
+            result = depth;
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i] && graph[node][i] == 1) {
+                dfs(i, depth + 1);
             }
         }
     }
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        x = Integer.parseInt(st.nextToken());
-        y = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(br.readLine());
-
-        visited = new boolean[N + 1];
-
-        for(int i = 0; i < N + 1; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for(int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            graph.get(a).add(b);
-            graph.get(b).add(a);
-        }
-        dfs(x, y, 0);
-
-        System.out.println(cnt);
-    }
 }
