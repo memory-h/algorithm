4f8afbf76090a3b9509831a044718aa1f6a5f846
memory-h <gusdud9124@gmail.com>
2025-09-19 18:07:40 +0900

    1260 DFS와 BFS

--- src/main/java/dfs/silver/silver_2/_1260.java
+++ src/main/java/dfs/silver/silver_2/_1260.java
@@ -1,92 +1,77 @@
 package dfs.silver.silver_2;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
+
 public class _1260 {
-    static StringBuilder sb = new StringBuilder();
-    static boolean[] check;
-    static int[][] arr;
-    static int node, N, M, V;
 
-    static Queue<Integer> q = new LinkedList<>();
+    private static int n;
+    private static int[][] graph;
+    private static boolean[] visited;
+    private static StringBuilder sb;
 
-    public static void dfs(int V) {
-        check[V] = true;
-        sb.append(V + " "); // 첫번째 값이 들어갔으므로 sb에 저장
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
-        for(int i = 1; i <= N; i++){
-            if(arr[V][i] == 1 && !check[i]){  // 탐색하지 않은 수들 확인
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        graph = new int[n + 1][n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[x][y] = graph[y][x] = 1;
+        }
+
+        visited = new boolean[n + 1];
+        dfs(v);
+
+        sb.append("\n");
+
+        visited = new boolean[n + 1];
+        bfs(v);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static void bfs(int v) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(v);
+        visited[v] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            sb.append(node).append(" ");
+
+            for (int i = 1; i <= n; i++) {
+                if (!visited[i] && graph[node][i] == 1) {
+                    visited[i] = true;
+                    queue.offer(i);
+                }
+            }
+        }
+    }
+
+    private static void dfs(int v) {
+        visited[v] = true;
+        sb.append(v).append(" ");
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i] && graph[v][i] == 1) {
+                visited[i] = true;
                 dfs(i);
             }
         }
     }
-    public static void bfs(int V) {
-        q.offer(V);
-        check[V] = true;
 
-        while(!q.isEmpty()){
-            V = q.poll();   // Queue의 맨 앞의 값을 빼주고
-            sb.append(V + " ");  // sb에 저장함
-
-            for(int i = 1; i <= N; i++){
-                if(arr[V][i] == 1 && !check[i]){  // 탐색하지 않은 수들 확인
-                    q.offer(i);         // Queue 성질 이용 없으면 넣고 check해준다
-                    check[i] = true;
-                }
-            }
-        }
-    }
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());   // 정점 개수
-        M = Integer.parseInt(st.nextToken());   // 간선 개수
-        V = Integer.parseInt(st.nextToken());   // 시작할 정점 번호
-
-        arr = new int[N + 1][N + 1];
-        check = new boolean[N + 1];   // 탐색한 내용을 다시 탐색하지 않도록 설정
-
-        for(int i = 0; i < M; i++){
-            st = new StringTokenizer(br.readLine());
-
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            arr[a][b] = arr[b][a] = 1;  // a -> b && b -> 로 갈 수 있다는 코드
-        }
-        dfs(V);
-        sb.append('\n');
-
-        check = new boolean[N + 1]; // bfs에서 check해준 값을 가져옴
-        bfs(V);
-
-        System.out.println(sb);
-    }
 }
-/*
-Stack<Integer> stack = new Stack<>();
-        visited = new boolean[N + 1];
-
-        stack.push(V);
-
-        while (!stack.isEmpty()){
-            int tmp = stack.pop();
-
-            if(visited[tmp]){
-                continue;
-            }
-            visited[tmp] = true;
-            sb.append(tmp + " ");
-
-            for(int i = 1; i <= N; i++){
-                if(!visited[i] && arr[tmp][i] == 1){
-                    stack.push(i);
-                }
-            }
-        }
-    }
- */
