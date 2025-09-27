43508ddc50dc350c6c72f522637b7e2a3e3798ce
memory-h <gusdud9124@gmail.com>
2025-09-27 17:41:10 +0900

    14248 점프 점프

--- src/main/java/bfs/silver/silver_2/_14248.java
+++ src/main/java/bfs/silver/silver_2/_14248.java
@@ -7,36 +7,11 @@ import java.util.StringTokenizer;
 
 public class _14248 {
 
-    static int n, count;
-    static int[] graph;
-    static boolean[] visited;
-    static int[] dx = {1, -1}; // 좌, 우 이동 가능
+    private static int n;
+    private static int count;
 
-    private static void bfs(int start) {
-
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
-        visited[start] = true;
-
-        while (!queue.isEmpty()) {
-            int pos = queue.poll();
-
-            for (int i = 0; i < 2; i++) {
-                int nx = pos + (dx[i] * graph[pos]);
-
-                if (nx < 0 || nx >= n) {
-                    continue;
-                }
-
-                if (!visited[nx]) {
-                    visited[nx] = true;
-                    queue.offer(nx);
-                    count++;
-                }
-            }
-        }
-
-    }
+    private static int[] stones;
+    private static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -45,21 +20,45 @@ public class _14248 {
 
         n = Integer.parseInt(br.readLine());
 
-        graph = new int[n];
+        stones = new int[n];
         visited = new boolean[n];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) graph[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            stones[i] = Integer.parseInt(st.nextToken());
+        }
 
         int s = Integer.parseInt(br.readLine());
 
-        count = 1;
-
-        // 출발점부터 bfs 탐색 시작
         bfs(s - 1);
 
-        bw.write(String.valueOf(count));
+        bw.write(String.valueOf(count + 1));
         bw.flush();
         bw.close();
     }
+
+    private static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            int right = cur + stones[cur];
+            int left = cur - stones[cur];
+
+            if (right < n && !visited[right]) {
+                visited[right] = true;
+                queue.offer(right);
+                count++;
+            }
+
+            if (left >= 0 && !visited[left]) {
+                visited[left] = true;
+                queue.offer(left);
+                count++;
+            }
+        }
+    }
+
 }
