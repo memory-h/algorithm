bf3e70738c04b281fe8b677a05a4c6b71ed079f4
memory-h <gusdud9124@gmail.com>
2025-09-28 22:54:27 +0900

    14496 그대, 그머가 되어

--- src/main/java/bfs/silver/silver_2/_14496.java
+++ src/main/java/bfs/silver/silver_2/_14496.java
@@ -5,66 +5,69 @@ import java.util.*;
 
 public class _14496 {
 
-    static int a, b, n, m;
-    static List<Integer>[] graph;
-    static boolean[] visited;
+    private static int n;
+    private static int m;
+    private static int result = -1;
 
-    /** 가중치가 없는 최단경로 구하는 문제 : BFS */
-    private static String bfs() {
-
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{a, 0});
-        visited[a] = true;
-
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-
-            if (now[0] == b) {
-                return String.valueOf(now[1]);
-            }
-
-            for (Integer i : graph[now[0]]) {
-                if (!visited[i]) {
-                    visited[i] = true;
-                    queue.offer(new int[]{i, now[1] + 1});
-                }
-            }
-        }
-
-        return "-1";
-    }
+    private static List<Integer>[] graph;
+    private static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
         graph = new List[n + 1];
-        for (int i = 0; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
         visited = new boolean[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new LinkedList<>();
+        }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-            // 양방향 연결
-            graph[u].add(v);
-            graph[v].add(u);
+            graph[x].add(y);
+            graph[y].add(x);
         }
 
-        bw.write(bfs());
+        bfs(a, b);
+
+        bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
     }
+
+    private static void bfs(int startNode, int endNode) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{startNode, 0});
+        visited[startNode] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            if (cur[0] == endNode) {
+                result = cur[1];
+                return;
+            }
+
+            for (int node : graph[cur[0]]) {
+                if (!visited[node]) {
+                    visited[node] = true;
+                    queue.offer(new int[]{node, cur[1] + 1});
+                }
+            }
+        }
+    }
+
 }
