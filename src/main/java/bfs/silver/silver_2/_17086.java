483433082703b565da00402d8d6fcf9ec78cfff6
memory-h <gusdud9124@gmail.com>
2025-09-30 00:08:05 +0900

    17086 아기 상어 2

--- src/main/java/bfs/silver/silver_2/_17086.java
+++ src/main/java/bfs/silver/silver_2/_17086.java
@@ -7,42 +7,14 @@ import java.util.StringTokenizer;
 
 public class _17086 {
 
-    static int n, m;
-    static int[][] graph;
-    static boolean[][] visited;
-    static int[] dx = {1, 0, -1, 0, 1, -1, -1, 1};
-    static int[] dy = {0, 1, 0, -1, 1, 1, -1, -1};
+    private static int n;
+    private static int m;
 
-    private static int bfs(int x, int y) {
+    private static int[][] graph;
+    private static boolean[][] visited;
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y, 0});
-        visited = new boolean[n][m];
-        visited[x][y] = true;
-
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-
-            for (int i = 0; i < 8; i++) {
-                int nx = dx[i] + now[0];
-                int ny = dy[i] + now[1];
-                int distance = now[2];
-
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny]) {
-                    continue;
-                }
-
-                // 아기 상어와 만났으면 거리 1을 추가하고 return
-                if (graph[nx][ny] == 1) return distance + 1;
-
-                visited[nx][ny] = true;
-                queue.offer(new int[]{nx, ny, distance + 1});
-
-            }
-        }
-
-        return 0;
-    }
+    private static final int[] dx = {1, -1, 0, 0, 1, -1, 1, -1};
+    private static final int[] dy = {0, 0, 1, -1, 1, -1, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -62,20 +34,50 @@ public class _17086 {
             }
         }
 
-        int max = 0;
+        int result = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-
-                // n * m 크기의 공간에 아기 상어가 아닐 때
-                if (graph[i][j] != 1) {
-                    max = Math.max(max, bfs(i, j));
+                if (graph[i][j] == 0) {
+                    visited = new boolean[n][m];
+                    result = Math.max(result, bfs(i, j));
                 }
             }
         }
 
-        bw.write(String.valueOf(max));
+        bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
     }
+
+    private static int bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y, 0});
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            for (int i = 0; i < 8; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+                    continue;
+                }
+
+                if (!visited[nx][ny] && graph[nx][ny] == 1) {
+                    return cur[2] + 1;
+                }
+
+                if (!visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    queue.offer(new int[]{nx, ny, cur[2] + 1});
+                }
+            }
+        }
+
+        return 0;
+    }
+
 }
