0366fab6ea8b0f83ea2c48db0b61584807ca76b0
memory-h <gusdud9124@gmail.com>
2025-12-01 16:56:22 +0900

    25416 빠른 숫자 탐색

--- src/main/java/bfs/silver/silver_2/_25416.java
+++ src/main/java/bfs/silver/silver_2/_25416.java
@@ -7,12 +7,12 @@ import java.util.StringTokenizer;
 
 public class _25416 {
 
-    static int r, c;
-    static int[][] graph;
-    static boolean[][] visited;
+    private static int r, c;
+    private static int[][] graph;
+    private static boolean[][] visited;
 
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,7 +35,6 @@ public class _25416 {
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
 
-        // 학생의 현재 위치(r, c)부터 bfs 탐색 시작
         int result = bfs(r, c);
 
         bw.write(String.valueOf(result));
@@ -44,50 +43,46 @@ public class _25416 {
     }
 
     private static int bfs(int x, int y) {
-
-        Queue<Board> queue = new LinkedList<>();
-        queue.add(new Board(x, y, 0));
-        visited[x][y] = true;
+        Queue<Position> queue = new LinkedList<>();
+        queue.offer(new Position(x, y, 0));
 
         while (!queue.isEmpty()) {
-            Board board = queue.poll();
+            Position curPosition = queue.poll();
+
+            if (graph[curPosition.x][curPosition.y] == 1) {
+                return curPosition.depth;
+            }
 
             for (int i = 0; i < 4; i++) {
-                int nx = dx[i] + board.x;
-                int ny = dy[i] + board.y;
+                int nx = curPosition.x + dx[i];
+                int ny = curPosition.y + dy[i];
 
-                // 범위를 벗어나거나 이동할 수 없는 칸인 경우
                 if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5 || graph[nx][ny] == -1) {
                     continue;
                 }
 
-                // 1이 적혀있는 칸에 도착하면 이동 횟수를 return
-                if (graph[nx][ny] == 1) {
-                    return board.count + 1;
-                }
-
                 if (!visited[nx][ny]) {
                     visited[nx][ny] = true;
-                    queue.add(new Board(nx, ny, board.count + 1));
+                    queue.offer(new Position(nx, ny, curPosition.depth + 1));
                 }
             }
-
         }
 
-        // 1이 적혀 있는 칸으로 이동할 수 없는 경우 -1 return
         return -1;
     }
 
-    static class Board {
+    private static class Position {
+
         private final int x;
         private final int y;
-        private final int count;
+        private final int depth;
 
-        public Board(int x, int y, int count) {
+        public Position(int x, int y, int depth) {
             this.x = x;
             this.y = y;
-            this.count = count;
+            this.depth = depth;
         }
+
     }
 
 }
