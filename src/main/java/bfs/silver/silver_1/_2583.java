8629ceb32dc447f3d0bc108f3e3e96cd76616ceb
memory-h <gusdud9124@gmail.com>
2025-12-10 00:00:10 +0900

    2583 영역 구하기

--- src/main/java/bfs/silver/silver_1/_2583.java
+++ src/main/java/bfs/silver/silver_1/_2583.java
@@ -1,82 +1,107 @@
 package bfs.silver.silver_1;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
 import java.util.*;
+
 public class _2583 {
-    static Queue<int []> q = new LinkedList<>();
-    static int N, M;
-    static int[][] graph;
-    static boolean[][] visited;
-    static int[] dx = new int[]{1, 0, 0, -1};
-    static int[] dy = new int[]{0, 1, -1, 0};
 
-    public static int bfs(int x, int y){
-        q.offer(new int[]{x, y});
-        visited[x][y] = true;
-        int cnt = 1;
+    private static int m, n;
+    private static int[][] graph;
+    private static boolean[][] visited;
 
-        while(!q.isEmpty()){
-            int[] xy = q.poll();
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
 
-            for(int i = 0; i < 4; i++){
-                int cx = xy[0] + dx[i];
-                int cy = xy[1] + dy[i];
-
-                if(cx >= 0 && cy >= 0 && cx < N && cy < M){
-                    if(!visited[cx][cy] && graph[cx][cy] == 0){
-                        visited[cx][cy] = true;
-                        graph[cx][cy] = graph[x][y] + 1;
-                        q.offer(new int[] {cx, cy});
-                        cnt++;
-                    }
-                }
-            }
-        }
-        return cnt;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> list = new ArrayList<>();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        graph = new int[N + 1][M + 1];
-        visited = new boolean[N + 1][M + 1];
+        graph = new int[m][n];
+        visited = new boolean[m][n];
 
-        for(int i = 0; i < K; i++){ // 좌표 안에 있는 곳들을 전부 1로 바꿈
+        for (int i = 0; i < k; i++) {
             st = new StringTokenizer(br.readLine());
 
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken()) - 1;
+            int y2 = Integer.parseInt(st.nextToken()) - 1;
 
-            for(int j = y1; j < y2; j++){
-                for(int k = x1; k < x2; k++){
-                    graph[j][k] = 1;
+            for (int y = y1; y <= y2; y++) {
+                for (int x = x1; x <= x2; x++) {
+                    graph[y][x] = 1;
                 }
             }
         }
-        int count = 0;
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                if(!visited[i][j] && graph[i][j] == 0){
-                    int result = bfs(i, j);
-                    list.add(result);
-                    count++;
+
+        List<Integer> areas = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        int totalAreaCount = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j] && graph[i][j] == 0) {
+                    areas.add(bfs(i, j));
+                    totalAreaCount++;
                 }
             }
         }
-        System.out.println(count);
 
-        Collections.sort(list);
+        Collections.sort(areas);
 
-        for(int i = 0; i < list.size(); i++){
-            System.out.print(list.get(i) + " ");
+        for (int area : areas) {
+            sb.append(area).append(" ");
         }
+
+        bw.write(totalAreaCount + "\n" + sb);
+        bw.flush();
+        bw.close();
     }
+
+    private static int bfs(int x, int y) {
+        Queue<Position> queue = new LinkedList<>();
+        queue.offer(new Position(x, y));
+        visited[x][y] = true;
+        int areaCount = 1;
+
+        while (!queue.isEmpty()) {
+            Position position = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = position.x + dx[i];
+                int ny = position.y + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= m || ny >= n) {
+                    continue;
+                }
+
+                if (!visited[nx][ny] && graph[nx][ny] == 0) {
+                    visited[nx][ny] = true;
+                    areaCount++;
+                    queue.offer(new Position(nx, ny));
+                }
+            }
+        }
+
+        return areaCount;
+    }
+
+    private static class Position {
+
+        private final int x;
+        private final int y;
+
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+
 }
