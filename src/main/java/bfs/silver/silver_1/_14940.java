8c7ba00f75a582a5f6f7f5915c6f355ea38033c9
memory-h <gusdud9124@gmail.com>
2025-12-21 16:54:45 +0900

    14940 쉬운 최단거리

--- src/main/java/bfs/silver/silver_1/_14940.java
+++ src/main/java/bfs/silver/silver_1/_14940.java
@@ -1,47 +1,19 @@
 package bfs.silver.silver_1;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class _14940 {
 
-    static int n, m;
-    static int[][] graph;
-    static int[][] result;
-    static boolean[][] visited;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
+    private static int m, n;
+    private static int targetX, targetY;
 
-    private static void bfs(int x, int y) {
+    private static int[][] graph;
+    private static int[][] arr;
+    private static boolean[][] visited;
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y, 0});
-        result[x][y] = 0;
-        visited[x][y] = true;
-
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-
-            // 가로, 세로 탐색
-            for (int i = 0; i < 4; i++) {
-                int nx = dx[i] + now[0];
-                int ny = dy[i] + now[1];
-
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m || graph[nx][ny] == 0) {
-                    continue;
-                }
-
-                if (!visited[nx][ny]) {
-                    visited[nx][ny] = true;
-                    result[nx][ny] = now[2] + 1; // 목표지점까지의 거리를 result 배열에 저장
-                    queue.offer(new int[]{nx, ny, now[2] + 1});
-                }
-            }
-        }
-
-    }
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -53,45 +25,79 @@ public class _14940 {
         m = Integer.parseInt(st.nextToken());
 
         graph = new int[n][m];
-        result = new int[n][m];
+        arr = new int[n][m];
         visited = new boolean[n][m];
-
-        int startX = 0;
-        int startY = 0;
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
 
             for (int j = 0; j < m; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = -1;
 
-                // 목표지점 좌표 저장
                 if (graph[i][j] == 2) {
-                    startX = i;
-                    startY = j;
+                    targetX = i;
+                    targetY = j;
+                    arr[i][j] = 0;
+                }
+
+                if (graph[i][j] == 0) {
+                    arr[i][j] = 0;
                 }
             }
         }
 
-        // 목표지점부터 bfs 탐색 시작
-        bfs(startX, startY);
+        bfs(targetX, targetY);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                // 땅인 부분 중에서 도달할 수 없는 위치
-                if (graph[i][j] == 1 && result[i][j] == 0) {
-                    sb.append(-1).append(" ");
-                }
-                else {
-                    sb.append(result[i][j]).append(" ");
-                }
+                sb.append(arr[i][j]).append(" ");
             }
             sb.append("\n");
         }
-
 
         bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
+
+    private static void bfs(int x, int y) {
+        Queue<Position> queue = new LinkedList<>();
+        queue.offer(new Position(x, y, 0));
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            Position position = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int cx = position.x + dx[i];
+                int cy = position.y + dy[i];
+
+                if (cx < 0 || cy < 0 || cx >= n || cy >= m || graph[cx][cy] == 0) {
+                    continue;
+                }
+
+                if (!visited[cx][cy] && graph[cx][cy] == 1) {
+                    visited[cx][cy] = true;
+                    arr[cx][cy] = position.distance + 1;
+                    queue.offer(new Position(cx, cy, position.distance + 1));
+                }
+            }
+        }
+    }
+
+    private static class Position {
+
+        private final int x;
+        private final int y;
+        private final int distance;
+
+        public Position(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
+        }
+
+    }
+
 }
